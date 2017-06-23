223db3a5032a95486b6b4617279726672bdbbb53
davidjj76 <davidjj76@gmail.com>
2017-06-24 00:33:55 +0200

    Create Popular component

--- app/index.js
+++ app/index.js
@@ -1,16 +1,8 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-require('./index.css');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello React Training!
-      </div>
-    );
-  }
-}
+import App from './components/App';
+require('./index.css');
 
 ReactDOM.render(
   <App />,
