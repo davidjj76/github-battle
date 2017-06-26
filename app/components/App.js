9b24be69ee04f838f773abe567ddf1a409463180
David Jiménez <djimenez@ets.es>
2017-06-26 19:12:23 +0200

    Route application

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Nav from './Nav';
+import Home from './Home'
+import Battle from './Battle';
 import Popular from './Popular';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Popular />
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/battle" component={Battle} />
+            <Route path="/popular" component={Popular} />
+            <Route render={() => <p>Not Found</p>} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ app/components/Battle.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Nav from './Nav';
+import Home from './Home'
+import Popular from './Popular';
+
+class Battle extends Component {
+  render() {
+    return (
+      <div>BATTLE!</div>
+    );
+  }
+}
+
+export default Battle;
